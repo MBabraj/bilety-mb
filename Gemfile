ea1a77faa17ef5344f06e0f8dba39fb336cd2502
MBabraj <michalina.babraj@gmail.com>
2017-12-26 20:51:52 +0100

    change home index color, add navbar

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+#
+
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 
 group :development, :test do
   gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :tickets
-  root :to => "tickets#index"
+  root 'home#index'
+
+  #resources :tickets
+  #root :to => "tickets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
