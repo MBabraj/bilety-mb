73f749bc973d28b1c8083bdfa41ade931bee5ee8
MBabraj <michalina.babraj@gmail.com>
2017-12-27 15:17:39 +0100

    change forms in devise views, change navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  #get 'tickets/index'
 
-  #resources :tickets
-  #root :to => "tickets#index"
+  resources :tickets
+  root :to => "tickets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
